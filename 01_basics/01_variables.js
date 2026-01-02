15bcc23d1fc69c4350c8c29207d79f20a335793c
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-02 12:02:26 +0000

    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14453
+let accountEmail = "nishant@gmail.com"
+var accountPassword = "123456"
+accountCity = "Chennai"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
