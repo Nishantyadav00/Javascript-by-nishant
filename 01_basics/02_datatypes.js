4924e09341446b5b96fd7035e3c38382c3403a1f
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-02 15:59:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3) we are using nodejs , not browser
+
+let name = "Nishant"
+let age = 18
+let isloggedIn = false
+
+/* DataTypes
+number = 2 to power 52/53
+bigint
+string = ""
+boolean = true/false
+null = standalone value
+undefined =
+symbol = for unique component
+
+object = later in this series*/
+
+console.log(typeof undefined); // type is undefined
+console.log(typeof null); // type is object
