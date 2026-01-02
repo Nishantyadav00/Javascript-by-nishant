62fe69b03a847ccece50bb696e8b9d225020588c
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-02 12:04:04 +0000

    forget to add warning in variable for future

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,4 +4,6 @@ var accountPassword = "123456"
 accountCity = "Chennai"
 let accountState
 
+//dont use var because of block space and functional space issue 
+
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
