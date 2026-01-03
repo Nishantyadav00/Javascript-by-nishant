10de2853916dacfd0ed64d6d288780f366d0f3fe
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-03 10:46:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "99abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"33" = 33
+"33abc" = NaN
+"Null" = 0
+"true" = 1 , "false" = 0
+"Nishant" = NaN
+*/
+
+//SIMILARLY boolean and string
+let isloggedIn = 1
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log(booleanisloggedIn);
+
+/*
+1 = true , 0 = false
+"" = false
+"Nishant" = true */
+
+let scores = 33
+let Stringscore = String(scores);
+console.log(Stringscore);
+console.log(typeof(Stringscore));
+
