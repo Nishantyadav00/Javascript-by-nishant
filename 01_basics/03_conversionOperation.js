125cdfc176bd7041ecc27c93f68d126b7836c170
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-03 11:51:00 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "99abc"
+//let score = "99abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 /*
 "33" = 33
@@ -16,17 +16,40 @@ console.log(valueInNumber);
 */
 
 //SIMILARLY boolean and string
-let isloggedIn = 1
-let booleanisloggedIn = Boolean(isloggedIn);
-console.log(booleanisloggedIn);
+//let isloggedIn = 1
+//let booleanisloggedIn = Boolean(isloggedIn);
+//console.log(booleanisloggedIn);
 
 /*
 1 = true , 0 = false
 "" = false
 "Nishant" = true */
 
-let scores = 33
-let Stringscore = String(scores);
-console.log(Stringscore);
-console.log(typeof(Stringscore));
+//let scores = 33
+//let Stringscore = String(scores);
+//console.log(Stringscore);
+//console.log(typeof(Stringscore));
 
+// ***********Operations************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Nishant"
+let str2 = "Yadav"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
