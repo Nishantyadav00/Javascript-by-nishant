8c0eff27218400c03fe50e17ee85a5be972a7923
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-05 15:49:02 +0000

    figuring out and trying something

--- 01_basics/06_numsANDmaths.js
+++ 01_basics/06_numsANDmaths.js
@@ -2,7 +2,7 @@ const score = 400
 console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+console.log(balance);// it will create object for number and diff output will come
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(1)); // for this it will give output in decimal
