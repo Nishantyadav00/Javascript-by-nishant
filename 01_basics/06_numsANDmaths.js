8fa1c558aefedc5b9cced7d3e0e1779a88c296dd
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-05 15:29:16 +0000

    Maths in js

--- 01_basics/06_numsANDmaths.js
+++ 01_basics/06_numsANDmaths.js
@@ -13,3 +13,25 @@ console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // give output in +ve number
+console.log(Math.round(4.6)); // basically round-off
+console.log(Math.ceil(4.2)); // it will give next value like 5 for this
+console.log(Math.floor(4.9)); // it will give same value like 4
+console.log(Math.min(4, 3, 6, 8)); //gives min value
+console.log(Math.max(4, 3, 6, 8)); // gives max value
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+console.log(Math.floor(Math.random()* 10 + 1)+ min);
