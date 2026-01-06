ef582ad7fa36234d4d2809edeaf88e368649af1e
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-06 18:41:46 +0000

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,5 +8,41 @@ if (true) {
 console.log(a);
 console.log(b);
 console.log(c);
-// console scope and node scope is different (interview question)
 
+// Note : console scope and node scope is different (interview question)
+
+function one(){
+    const username = "Nishant"
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website); // error
+    two()
+}
+one()
+
+if (true) {
+    const username = "Nishant"
+    if (username === "Nishant") {
+        const website = "Google"
+        console.log(username + website);
+    }
+    // console.log(website); // error
+}
+// console.log(username); // error
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // this will not give error in output
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)// this will give error in output because of the declaration rule (hoisting)
+const addTwo = function(num){
+    return num + 2
+}
