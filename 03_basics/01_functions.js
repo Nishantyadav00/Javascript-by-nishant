0a5e1d00bc8a9dc315c84363f282598559c5f151
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-06 17:22:51 +0000

    functions with object and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,8 +7,7 @@ function sayMyName(){
     console.log("N");
     console.log("T");
 }
-
-// sayMyName // its for reference , sayMyName() // its for execution
+sayMyName()// sayMyName is for reference , sayMyName() is for execution
 
 // function addTwoNumbers(number1, number2){
 //     console.log(number1 + number2);
@@ -19,11 +18,9 @@ function addTwoNumbers(number1, number2){
     // return result
     return number1 + number2
 }
-
+// Note : after return we cant fetch anything 
 const result = addTwoNumbers(3, 5)
-
-// console.log("Result: ", result);
-
+console.log("Result: ", result);
 
 function loginUserMessage(username = "sam"){
     if(!username){
@@ -33,5 +30,36 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("Nishant"))
-// console.log(loginUserMessage("Nishant"))
+console.log(loginUserMessage("Nishant"))
+
+
+// ... is also called rest operator on the basis of use it is determined that whether it is spread or rest
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Nishant",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
