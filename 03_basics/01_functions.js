ccbcabfcd3a4579998d044c7109d710ef67c6152
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-06 17:08:47 +0000

    functions and parameters in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,37 @@
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("N");
+    console.log("T");
+}
+
+// sayMyName // its for reference , sayMyName() // its for execution
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Nishant"))
+// console.log(loginUserMessage("Nishant"))
