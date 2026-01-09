c25655df5c2cac2ab5d52398c8d307113929d07e
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-09 15:08:08 +0000

    added comment

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,10 +46,10 @@ let str3 = str1 + str2
 
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
 
 console.log(true);
 console.log(+true);
