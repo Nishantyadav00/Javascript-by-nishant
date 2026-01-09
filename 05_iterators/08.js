8d069e4f653e28b23429a6e9cc854aac18b39bc6
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-09 18:31:07 +0000

    added comment to reduce

--- 05_iterators/08.js
+++ 05_iterators/08.js
@@ -5,6 +5,10 @@ const myNums = [1, 2, 3]
 //     return acc + currval
 // }, 0)
 
+// reduce takes two arguments , first is a callback function and second is initial value of accumulator , if not provided then first element of array is taken as initial value and iteration starts from second element
+
+// it basically adds each element of array and returns a single value
+
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 console.log(myTotal);
