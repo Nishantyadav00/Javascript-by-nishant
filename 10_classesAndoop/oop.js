a0b44abbb91514d1c3a8eaf212ed40fd6f25f6be
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-14 07:52:59 +0000

    object oriented in js

---
+++ 10_classesAndoop/oop.js
@@ -0,0 +1,42 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
+
+// working of new keyword
+// step 1 -whenever a new keyword is used , an empty object is created which is known as instance 
+// step 2 - a constructor function is called with this keyword pointing to the newly created object
+// step 3 - using this keyword all arguments are injected inside the newly created object
+// step 4 - at last the newly created object is returned from the constructor function automatically
