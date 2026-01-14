7032438580d5bb2f3aa3cc6cb13638b77ce14590
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-14 09:06:50 +0000

    modern syntax of inheritance in js

--- 10_classesAndoop/Prototype.js
+++ 10_classesAndoop/Prototype.js
@@ -48,3 +48,18 @@ const TASupport = {
     __proto__: TeachingSupport
 }
 Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Nishant".trueLength()
+"iceTea".trueLength()
