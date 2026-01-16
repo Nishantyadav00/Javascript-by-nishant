efc218af12892c66e0222cc4ccf4a1f98de6a756
Nishantyadav00 <nishantyadav954080@gmail.com>
2026-01-16 09:25:53 +0000

    properties getter setter via objects in js

---
+++ 10_classesAndoop/Object_get_set.js
@@ -0,0 +1,16 @@
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)// object.create are factory functions and new are constructor functions
+console.log(tea.email);
